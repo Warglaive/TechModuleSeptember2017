79bf3f506826ddf089dd47dfeeb021fe4d7d10bb
warglaive <warglaivegm@gmail.com>
2017-09-19 17:57:43 +0300

    directory fix

--- C# Intro and Basic Syntax - Lab/03. Employee Data/03. Employee Data.cs
+++ C# Intro and Basic Syntax - Lab/03. Employee Data/03. Employee Data.cs
@@ -10,11 +10,7 @@ namespace _03.Employee_Data
             var age = int.Parse(Console.ReadLine());
             var id = int.Parse(Console.ReadLine());
             var salary = double.Parse(Console.ReadLine());
-            Console.WriteLine("Name: {0}" +
-                Environment.NewLine +
-                "Age: {1}" + Environment.NewLine +
-                "Employee ID: {2:d8}" +
-                Environment.NewLine+ "Salary: {3:f2}",name, age, id, salary);
+            Console.WriteLine("Name: {0}" + Environment.NewLine + "Age: {1}" + Environment.NewLine +  "Employee ID: {2:d8}" +  Environment.NewLine+ "Salary: {3:f2}",name, age, id, salary);
         }
     }
 }
