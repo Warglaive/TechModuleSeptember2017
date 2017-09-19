673089744454c2364834fbe0f6423eb2151981e5
warglaive <warglaivegm@gmail.com>
2017-09-19 19:09:41 +0300

    star Task Done

--- C# Intro and Basic Syntax - Exercises/CSharIntroExercises/05. Character Stats/05. Character Stats.cs
+++ C# Intro and Basic Syntax - Exercises/CSharIntroExercises/05. Character Stats/05. Character Stats.cs
@@ -1,16 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _05.Character_Stats
 {
-   public class Program
+    public class Program
     {
        public static void Main()
         {
+            var name = Console.ReadLine();
+            var currentHealth = int.Parse(Console.ReadLine());
+            var maximumHealth = int.Parse(Console.ReadLine());
+            var currentEnergy = int.Parse(Console.ReadLine());
+            var maximumEnergy = int.Parse(Console.ReadLine());
 
+            Console.WriteLine($"Name: {name}");
+
+            Console.Write("Health: |");
+                for (int i = 0; i < currentHealth; i++)
+                {
+                Console.Write("|");
+                }
+            for (int i = 0; i < maximumHealth-currentHealth; i++)
+            {
+                Console.Write(".");
+            }
+            Console.WriteLine("|");
+            //Energy
+            Console.Write("Energy: |");
+            for (int i = 0; i < currentEnergy; i++)
+            {
+                Console.Write("|");
+            }
+            for (int i = 0; i < maximumEnergy-currentEnergy; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write("|");
         }
     }
 }
