4848d2b8ffbf8130fbdc7b9a0f76a5b93466b87a
warglaive <warglaivegm@gmail.com>
2017-09-19 19:52:40 +0300

    new string solution

---
+++ C# Intro and Basic Syntax - Exercises/CSharIntroExercises/CharStatSecondSoluti/CharStatSecondSoluti.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharStatSecondSoluti
+{
+   public class CharStatSecondSoluti
+    {
+       public static void Main()
+        {
+            var name = Console.ReadLine();
+            var currentHealth = int.Parse(Console.ReadLine());
+            var maximumHealth = int.Parse(Console.ReadLine());
+            var currentEnergy = int.Parse(Console.ReadLine());
+            var maximumEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+
+            Console.Write("Health: |");
+            Console.Write(new string('|', currentHealth));
+            Console.Write(new string('.', maximumHealth-currentHealth));
+            Console.Write("|");
+            //Energy
+            Console.WriteLine();
+            Console.Write("Energy: |");
+            Console.Write(new string('|', currentEnergy));
+            Console.Write(new string('.', maximumEnergy-currentEnergy));
+            Console.Write("|");
+        }
+    }
+}
