c4241cf1ff008daa58936c0ded492a35bb083f15
warglaive <warglaivegm@gmail.com>
2017-09-22 21:23:13 +0300

    to finish 04. Hotel

---
+++ STATEMENTS AND LOOPS - EXERCISES/LoopsIFExcercise/03. Restaurant Discount/03. Restaurant Discount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _03.Restaurant_Discount
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var groupSize = int.Parse(Console.ReadLine());
+            var packType = Console.ReadLine();
+            var pricePerPerson = 0d;
+            var discount = 0d;
+            var hallPrice = 0d;
+            var TotalPrice = 0d;
+            var HallType = string.Empty;
+
+            if (groupSize <= 50)
+            {
+                hallPrice = 2500;
+                HallType = "Small Hall";
+            }
+            else if (groupSize > 50 && groupSize <= 100) 
+            {
+                hallPrice = 5000;
+                HallType = "Terrace";
+            }
+            else if (groupSize > 100 && groupSize <= 120)
+            {
+                hallPrice = 7500;
+                HallType = "Great Hall";
+            }
+            else
+            {
+                Console.WriteLine("We do not have an appropriate hall.");
+                return;
+            }
+            //
+            if (packType== "Normal")
+            {
+                discount = (hallPrice + 500) * 0.05;
+                TotalPrice = (hallPrice + 500) - discount;
+                pricePerPerson = TotalPrice / groupSize;
+                Console.WriteLine($"We can offer you the {HallType + Environment.NewLine}The price per person is {pricePerPerson:f2}$");
+            }
+            else if (packType=="Gold")
+            {
+                discount = (hallPrice + 750) * 0.10;
+                TotalPrice = (hallPrice + 750) - discount;
+                pricePerPerson = TotalPrice / groupSize;
+                Console.WriteLine($"We can offer you the {HallType + Environment.NewLine}The price per person is {pricePerPerson:f2}$");
+            }
+            else if (packType == "Platinum")
+            {
+                discount = (hallPrice + 1000) * 0.15;
+                TotalPrice = (hallPrice + 1000) - discount;
+                pricePerPerson = TotalPrice / groupSize;
+                Console.WriteLine($"We can offer you the {HallType + Environment.NewLine}The price per person is {pricePerPerson:f2}$");
+            }
+        }
+    }
+}
---
+++ STATEMENTS AND LOOPS - EXERCISES/LoopsIFExcercise/04. Hotel/04. Hotel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Hotel
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var month = Console.ReadLine();
+            var nightsCount = int.Parse(Console.ReadLine());
+
+            var studioPrice = 0d;
+            var doublePrice = 0d;
+            var suitePrice = 0d;
+
+            if (month == "May" || month == "October")
+            {
+                studioPrice = 50;
+                doublePrice = 65;
+                suitePrice = 75;
+            }
+            else if (month == "June" || month == "September")
+            {
+                studioPrice = 60;
+                doublePrice = 72;
+                suitePrice = 82;
+            }
+            else if (month == "July" || month == "August" || month == "December")
+            {
+                studioPrice = 68;
+                doublePrice = 77;
+                suitePrice = 89;
+            }
+            //discount
+            if (nightsCount > 7 && (month == "May" || month == "October"))
+            {
+                studioPrice = (studioPrice * 0.95) * nightsCount; //5%
+                doublePrice *= nightsCount;
+                suitePrice *= nightsCount;
+            }
+            else if (nightsCount > 14 && (month == "June" || month == "September"))
+            {
+                doublePrice = (doublePrice * 0.90) * nightsCount; //10%
+                studioPrice *= nightsCount;
+                suitePrice *= nightsCount;
+            }
+            else if (nightsCount > 14 && (month == "July" || month == "August" || month == "December"))
+            {
+                suitePrice = (suitePrice * 0.85) * nightsCount; //15%
+                doublePrice *= nightsCount;
+                studioPrice *= nightsCount;
+            }
+            if (nightsCount > 7 && (month == "September" || month == "October"))  //maybe simple if, not else if
+            {
+                if (month == "September")
+                {
+                    studioPrice = (studioPrice * nightsCount) - studioPrice;
+                    doublePrice *= nightsCount;
+                    suitePrice *= nightsCount;
+                }
+                else
+                {
+                    studioPrice = (studioPrice * nightsCount) - studioPrice;
+                    doublePrice *= nightsCount;
+                    suitePrice *= nightsCount;
+                }
+            }
+            else
+            {
+                studioPrice *= nightsCount;
+                doublePrice *= nightsCount;
+                suitePrice *= nightsCount;
+            }
+            Console.WriteLine($"Studio: {studioPrice:f2} lv." +
+                Environment.NewLine + $"Double: {doublePrice:f2} lv." +
+                Environment.NewLine + $"Suite: {suitePrice:f2} lv.");
+        }
+    }
+}
