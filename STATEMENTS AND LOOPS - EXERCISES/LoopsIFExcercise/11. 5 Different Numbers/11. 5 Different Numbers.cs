4d70acb661f99d4fc5f22517c663d7d6be16ff65
warglaivee <warglaivegm@gmail.com>
2017-09-24 15:53:29 +0300

    11. 5 Different Numbers 50% rdy

---
+++ STATEMENTS AND LOOPS - EXERCISES/LoopsIFExcercise/11. 5 Different Numbers/11. 5 Different Numbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._5_Different_Numbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i < 5; i++)//first check if satisfied
+            {
+                if (a <= i)
+                {
+                    for (int k = i; k < 5; k++)
+                    {
+                        i++;
+                        if (k < i)
+                        {
+                            for (int l = i; l < 5; l++)
+                            {
+                                i++;
+                                if (l < i)
+                                {
+                                    for (int m = i; m <= 5; m++)
+                                    {
+                                        if (m <= i)
+                                        {
+                                            Console.WriteLine($"{a} {i} {k} {l} {m}");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }//then print
+        }
+    }
+}
