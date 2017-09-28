ee0256b95f876fa48495200ee4b0287419d0f354
warglaivee <warglaivegm@gmail.com>
2017-09-28 19:45:16 +0300

    some progres

---
+++ CSharp Basics - More Exercises/01. X/01. X.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.X
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var formula = (n - 2) - 2;
+            Console.Write('x');//firstLine
+            Console.Write(new string(' ', n - 2));
+            Console.WriteLine('x');
+
+            for (int i = 1; i < n / 2; i++)
+            {
+                Console.Write(new string(' ', i));
+                Console.Write('x');
+                Console.Write(new string(' ', ((n - 2) - i * 2)));
+                Console.Write('x');
+                Console.WriteLine(new string(' ', i));
+
+            }
+
+            Console.Write('x');//lastLineine
+            Console.Write(new string(' ', n - 2));
+            Console.WriteLine('x');
+        }
+    }
+}
