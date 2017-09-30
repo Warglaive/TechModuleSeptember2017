3c91d6cb3509cc81515e829caa65e5a679dc64f0
warglaivee <warglaivegm@gmail.com>
2017-09-30 17:45:18 +0300

    06. DNA Sequences a bit hard

--- CSharp Basics - More Exercises/05. BPM Counter/Program.cs
+++ CSharp Basics - More Exercises/05. BPM Counter/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _05.BPM_Counter
 {
@@ -10,10 +6,10 @@ namespace _05.BPM_Counter
     {
         public static void Main()
         {
-            var BeatsPerMinute = double.Parse(Console.ReadLine());
+            var beatsPerMinute = double.Parse(Console.ReadLine());
             var numberOfBeats = double.Parse(Console.ReadLine());
             Console.Write($"{Math.Round(numberOfBeats/4.0, 1)} bars - ");
-            var beatsPerSecond = (numberOfBeats * 60) / BeatsPerMinute;
+            var beatsPerSecond = (numberOfBeats * 60) / beatsPerMinute;
             var min = (int)beatsPerSecond / 60;
             beatsPerSecond -= min * 60;
             Console.WriteLine($"{min}m {Math.Floor(beatsPerSecond)}s");
---
+++ CSharp Basics - More Exercises/06. DNA Sequences/06. DNA Sequences.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _06.DNA_Sequences
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var lineCounter = 0;
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int k = 1; k <= 4; k++)
+                {
+                    for (int l = 1; l <= 4; l++)
+                    {
+                        var firstChar = string.Empty;
+                        var secondChar = string.Empty;
+                        var thirdChar = string.Empty;
+                        if (i == 1)
+                        {
+                            firstChar = "A";
+                        }
+                        else if (i == 2)
+                        {
+                            firstChar = "C";
+                        }
+                        else if (i == 3)
+                        {
+                            firstChar = "G";
+                        }
+                        else
+                        {
+                            firstChar = "T";
+                        }
+                        //
+                        if (k == 1)
+                        {
+                            secondChar = "A";
+                        }
+                        else if (k == 2)
+                        {
+                            secondChar = "C";
+                        }
+                        else if (k == 3)
+                        {
+                            secondChar = "G";
+                        }
+                        else
+                        {
+                            secondChar = "T";
+                        }
+                        //
+                        if (l == 1)
+                        {
+                            thirdChar = "A";
+                        }
+                        else if (l == 2)
+                        {
+                            thirdChar = "C";
+                        }
+                        else if (l == 3)
+                        {
+                            thirdChar = "G";
+                        }
+                        else
+                        {
+                            thirdChar = "T";
+                        }
+                        var prefix = string.Empty;
+                        var suffix = string.Empty;
+                        if (i + k + l >= n)
+                        {
+                            prefix = "O";
+                            suffix = "O";
+                        }
+                        else
+                        {
+                            prefix = "X";
+                            suffix = "X";
+                        }
+                        lineCounter++;
+                        if (lineCounter == 4)
+                        {
+                            Console.WriteLine(prefix + firstChar + secondChar + thirdChar + suffix + " ");
+                            lineCounter = 0;
+                        }
+                        else
+                        {
+                            Console.Write(prefix + firstChar + secondChar + thirdChar + suffix + " ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
