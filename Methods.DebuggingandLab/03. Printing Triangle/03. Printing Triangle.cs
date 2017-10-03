542fa6b2f4367db28477621a7e894b345ef8b2ea
warglaive <warglaivegm@gmail.com>
2017-10-03 18:08:51 +0300

    refactor

--- Methods.DebuggingandLab/03. Printing Triangle/03. Printing Triangle.cs
+++ Methods.DebuggingandLab/03. Printing Triangle/03. Printing Triangle.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _03.Printing_Triangle
 {
@@ -11,18 +7,29 @@ namespace _03.Printing_Triangle
         static void Main()
         {
             var n = int.Parse(Console.ReadLine());
-            for (int i = 1; i < n; i++)
+
+            for (int i = 0; i < n; i++)
             {
-                PrintLine(i, n);
+                PrintLine(1, i);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                PrintLine(1, n - i);
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                PrintLine(1, i);
             }
 
         }
 
         private static void PrintLine(int start, int end)
         {
-            for (int i = start; i < end; i++)
+            for (int i = start; i <= end; i++)
             {
-                Console.WriteLine(i + " ");
+                Console.Write(i + " ");
             }
             Console.WriteLine();
         }
