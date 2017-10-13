baaf6d74796ef21f5490f5a4624d6bb7971fd72a
Warglaive <warglaivegm@gmail.com>
2017-10-13 16:10:39 +0300

    Add files via upload

---
+++ Arrays - Lab/04. Triple Sum/04. Triple Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Triple_Sum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (b)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
