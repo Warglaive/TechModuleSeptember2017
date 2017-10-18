6b59ea7f0c7d0e4ec30efffc85ea3938bf4ae1d5
warglaivee <warglaivegm@gmail.com>
2017-10-18 17:32:16 +0300

    08. Logs Aggregator done
    09. Legendary Farming copy/paste

---
+++ Dictionaries, Lambda and LINQ - Exercises/08. Logs Aggregator/08. Logs Aggregator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Logs_Aggregator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var result = new SortedDictionary<string, SortedDictionary<string, int>>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ').ToList();
+                var IP = input[0];
+                var user = input[1];
+                var duration = int.Parse(input[2]);
+
+                if (!result.ContainsKey(user))
+                {
+                    result.Add(user, new SortedDictionary<string, int>());
+                }
+                if (!result[user].ContainsKey(IP))
+                {
+                    result[user][IP] = 0;
+                }
+                result[user][IP] += duration;
+            }
+            foreach (var KvP in result)
+            {
+                Console.WriteLine($"{KvP.Key}: {KvP.Value.Values.Sum()} [{string.Join(", ", KvP.Value.Keys)}]");
+            }
+        }
+    }
+}
