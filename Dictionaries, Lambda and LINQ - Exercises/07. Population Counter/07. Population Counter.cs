232b58c8290aa300930fba91c6b0258b76d7da00
warglaivee <warglaivegm@gmail.com>
2017-10-18 15:26:47 +0300

    07. Population Counter funny

--- Dictionaries, Lambda and LINQ - Exercises/07. Population Counter/07. Population Counter.cs
+++ Dictionaries, Lambda and LINQ - Exercises/07. Population Counter/07. Population Counter.cs
@@ -10,7 +10,35 @@ namespace _07.Population_Counter
     {
         public static void Main()
         {
+            var countries = new Dictionary<string, Dictionary<string, long>>();
+            var input = Console.ReadLine().Split('|').ToList();
+            while (input[0] != "report")
+            {
+                var city = input[0];
+                var country = input[1];
+                var population = long.Parse(input[2]);
 
+                if (!countries.ContainsKey(country))
+                {
+                    countries.Add(country, new Dictionary<string, long>());
+                }
+                countries[country].Add(city, population);
+
+
+                input = Console.ReadLine().Split('|').ToList();
+            }
+            var sorted = countries
+                .OrderByDescending(n => n.Value.Values.Sum())
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            foreach (var KvP in sorted)
+            {
+                Console.WriteLine($"{KvP.Key} (total population: {KvP.Value.Values.Sum()})");
+                foreach (var cityAndPop in KvP.Value.OrderByDescending(x=>x.Value))
+                {
+                    Console.WriteLine($"=>{cityAndPop.Key}: {cityAndPop.Value}");
+                }
+            }
         }
     }
 }
