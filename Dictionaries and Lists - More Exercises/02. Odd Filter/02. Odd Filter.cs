9c2027bde283d7b8fc9912fd2d54d8de75949880
warglaivee <warglaivegm@gmail.com>
2017-10-21 15:56:22 +0300

    02. Odd Filter 100/100 reverse for

--- Dictionaries and Lists - More Exercises/02. Odd Filter/02. Odd Filter.cs
+++ Dictionaries and Lists - More Exercises/02. Odd Filter/02. Odd Filter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.Odd_Filter
 {
@@ -16,11 +14,11 @@ namespace _02.Odd_Filter
             Console.WriteLine(string.Join(" ", input));
         }
 
-        private static List<int> Convert(List<int> input)
+        private static void Convert(List<int> input)
         {
+            var average = Math.Ceiling(input.Average());
             for (int i = 0; i <= input.Count - 1; i++)
             {
-                var average = Math.Ceiling(input.Average());
                 if (input[i] > average)
                 {
                     input[i] += 1;
@@ -30,20 +28,18 @@ namespace _02.Odd_Filter
                     input[i] -= 1;
                 }
             }
-            return input;
         }
 
-        public static List<int> RemoveOddNumbers(List<int> input)
+        public static void RemoveOddNumbers(List<int> input)
         {
-            for (int i = 0; i <= input.Count - 1; i++)
+            for (int i = input.Count - 1; i >= 0; i--)
             {
                 if (input[i] % 2 != 0)
                 {
                     input.Remove(input[i]);
-                    i = 0;
+                    i = input.Count;
                 }
             }
-            return input;
         }
     }
 }
--- Dictionaries and Lists - More Exercises/03. Immune System/03. Immune System.cs
+++ Dictionaries and Lists - More Exercises/03. Immune System/03. Immune System.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _03.Immune_System
 {
@@ -14,23 +11,19 @@ namespace _03.Immune_System
             var healthStore = new List<double>();
             healthStore.Add(immuneSystemInitialHealth);
             var virusNames = new List<string>();
-            var virusStrength = 0;
-            var tempStrenght = 0;
-            var timeToDefeat = 0;
             var virusName = Console.ReadLine();
             while (virusName != "end")
             {
                 immuneSystemInitialHealth = healthStore[0];
-                virusStrength = 0;
-                tempStrenght = 0;
+                var tempStrenght = 0;
                 //virusStrenght
-
                 foreach (var letter in virusName)
                 {
                     tempStrenght += letter;
                 }
-                virusStrength = tempStrenght / 3;
+                var virusStrength = tempStrenght / 3;
                 //Time To Defeat - power * namelenght
+                var timeToDefeat = 0;
                 if (virusNames.Contains(virusName))
                 {
                     timeToDefeat = (virusStrength * virusName.Length) / 3;
@@ -40,10 +33,6 @@ namespace _03.Immune_System
                     timeToDefeat = virusStrength * virusName.Length;
                 }//calc healt
                 var currentHealth = healthStore[0] - timeToDefeat;
-                
-
-                
-                //
                 var timeInMinutes = TimeSpan.FromSeconds(timeToDefeat);
                 if (timeInMinutes.Hours > 0)
                 {
@@ -66,8 +55,8 @@ namespace _03.Immune_System
                     }
                     Console.WriteLine($"{virusName} defeated in {timeInMinutes.Minutes}m {timeInMinutes.Seconds}s.");
                 }
-                Console.WriteLine($"Remaining health: {currentHealth}");
-                healthStore[0] = currentHealth + (currentHealth * 0.20);
+                Console.WriteLine($"Remaining health: {Math.Floor(currentHealth)}");
+                healthStore[0] = (int)currentHealth + ((int)currentHealth * 0.20);
 
                 if (healthStore[0] > immuneSystemInitialHealth)
                 {
@@ -76,8 +65,10 @@ namespace _03.Immune_System
                 virusNames.Add(virusName);
                 virusName = Console.ReadLine();
             }
-            Console.WriteLine($"Final Health: {Math.Floor(healthStore[0])}");
+            if (virusName == "end")
+            {
+                Console.WriteLine($"Final Health: {(int)healthStore[0]}");
+            }
         }
-        // zakruglqneto da opraq inache bachka..
     }
 }
