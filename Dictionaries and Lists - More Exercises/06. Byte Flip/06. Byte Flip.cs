29c62225d939597a3ef8df88c965aa616932fb2f
warglaivee <warglaivegm@gmail.com>
2017-10-22 17:15:29 +0300

    06. Byte Flip wtf

---
+++ Dictionaries and Lists - More Exercises/06. Byte Flip/06. Byte Flip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Byte_Flip
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split()
+                .Where(digit => digit.Length == 2)
+                .Select(x => new string(x.Reverse().ToArray()))
+                .Select(x => Convert.ToChar(Convert.ToInt32(x, 16)))
+                .Reverse()
+                .ToList();
+            Console.WriteLine(string.Join("", input));
+        }
+    }
+}
