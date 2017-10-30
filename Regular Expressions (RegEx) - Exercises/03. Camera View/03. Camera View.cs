03a971d0254b0a17f83c23ad7e152d1f0c3ee4d0
warglaivee <warglaivegm@gmail.com>
2017-10-30 22:18:32 +0200

    04. Weather done 100/100

--- Regular Expressions (RegEx) - Exercises/03. Camera View/03. Camera View.cs
+++ Regular Expressions (RegEx) - Exercises/03. Camera View/03. Camera View.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 namespace _03.Camera_View
 {
@@ -10,7 +8,39 @@ namespace _03.Camera_View
     {
         public static void Main()
         {
+            var indices =
+                Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
 
+            var inputLine = Console.ReadLine();
+
+            int m = indices[0];
+            int n = indices[1];
+            var pattern = $"\\|\\<(.{{0,{m}}})(.{{0,{n}}})";
+            var regex = new Regex(pattern);
+
+            Match match = regex.Match(inputLine);
+
+            var result = new List<string>();
+
+            while (inputLine.Length > match.Index + 2 && match.Success)
+            {
+                string matchToAdd = match.Groups[2].Value;
+                if (matchToAdd.Contains("|"))
+                {
+                    matchToAdd = matchToAdd.Substring(0, matchToAdd.IndexOf("|"));
+                }
+                if (matchToAdd.Contains("<"))
+                {
+                    matchToAdd = matchToAdd.Substring(0, matchToAdd.IndexOf("<"));
+                }
+                result.Add(matchToAdd);
+                inputLine = inputLine.Substring(match.Index + 2);
+                match = regex.Match(inputLine);
+            }
+            Console.WriteLine(string.Join(", ", result));
         }
     }
 }
---
+++ Regular Expressions (RegEx) - Exercises/04. Weather/04. Weather.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace _04.Weather
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var pattern = @"(?<city>[A-Z]{2})(?<temp>\d{1,2}\.\d{1,2})([A-Za-z]+)(?=[|])";
+            var result = new Dictionary<string, Dictionary<double, string>>();
+            var input = Console.ReadLine();
+            while (input != "end")
+            {
+                foreach (Match wordMatch in Regex.Matches(input, pattern))
+                {
+                    var city = Convert.ToString(wordMatch.Groups["city"]);
+                    var temp = Convert.ToDouble(wordMatch.Groups["temp"].Value);
+                    var type = Convert.ToString(wordMatch.Groups[1]);
+
+                    if (!result.ContainsKey(city))
+                    {
+                        result.Add(city, new Dictionary<double, string>());
+                    }
+                    else
+                    {
+                        result.Remove(city); //key cant be rewrited - remove it and add it again
+                        result.Add(city, new Dictionary<double, string>());
+                    }
+                    if (!result[city].ContainsKey(temp))
+                    {
+                        result[city][temp] = type;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var CityPair in result.OrderBy(x => x.Value.Keys.Average()))
+            {
+                var city = CityPair.Key;
+                foreach (var tempTypePairs in CityPair.Value)
+                {
+                    var temp = tempTypePairs.Key;
+                    var type = tempTypePairs.Value;
+                    Console.WriteLine($"{city} => {temp:f2} => {type}");
+                }
+            }
+        }
+    }
+}
