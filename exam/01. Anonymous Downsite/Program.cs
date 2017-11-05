1a0acaa252b0a5d954debfc546319c6cf958bc8e
warglaive <warglaivegm@gmail.com>
2017-11-05 18:39:17 +0200

    first Part 100/200

---
+++ exam/01. Anonymous Downsite/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _01.Anonymous_Downsite
+{
+    class Program
+    {
+        static void Main()
+        {
+            var websiteNum = int.Parse(Console.ReadLine());
+            var securityKey = BigInteger.Parse(Console.ReadLine());
+
+            var affectedSitesNames = new List<string>();
+            var totalMoneyLoss = new List<decimal>();
+
+            for (int i = 0; i < websiteNum; i++)
+            {
+                var data = Console.ReadLine().Split(new[] { ' ' }
+                    , StringSplitOptions.RemoveEmptyEntries).ToList();
+                var siteName = data[0].Trim();
+                affectedSitesNames.Add(siteName);
+                var siteVisits = decimal.Parse(data[1]);
+                var siteCommercialPricePerVisit = decimal.Parse(data[2]);
+                var siteLoss = siteVisits * siteCommercialPricePerVisit;
+                totalMoneyLoss.Add(siteLoss);
+            }
+            foreach (var siteName in affectedSitesNames)
+            {
+                Console.WriteLine(siteName);
+            }
+            Console.WriteLine($"Total Loss: {totalMoneyLoss.Sum():f20}");
+            Console.WriteLine($"Security Token: {BigInteger.Pow(securityKey, websiteNum)}");
+        }
+    }
+}
---
+++ exam/02. Anonymous Threat/02. Anonymous Threat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Anonymous_Threat
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(new[] { ' ' }
+            , StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var result = new List<string>();
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+                if (inputLine == "3:1")
+                {
+                    break;
+                }
+                var currentLine = inputLine.Split(' ').ToList();
+                var command = currentLine[0];
+                var startIndex = int.Parse(currentLine[1]);
+                var endIndex = int.Parse(currentLine[2]);
+                switch (command)
+                {
+                    case "merge":
+                        result = Merge(input, startIndex, endIndex, result);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static List<string> Merge(List<string> input, int startIndex, int endIndex, List<string> result)
+        {
+            string currentWord = "";
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                currentWord += input[i];
+            }
+            input.RemoveRange(startIndex, endIndex + 1);
+            result.Add(currentWord);
+            input.Insert(0, currentWord);
+            return result;
+        }
+
+    }
+}
